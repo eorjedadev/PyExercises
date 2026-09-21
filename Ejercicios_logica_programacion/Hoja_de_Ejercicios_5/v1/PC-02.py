30d5d4b5be902e5e5c7f1d235a3e315a50fe7001
eorjedadev <e.orjeda.pro@gmail.com>
2026-09-21 16:18:24 -0500

    PC-02: Ejercicio for completado correctamente

---
+++ Ejercicios_logica_programacion/Hoja_de_Ejercicios_5/v1/PC-02.py
@@ -0,0 +1,36 @@
+#range(inicio, limite, paso)
+#inicio → dónde comienza
+#límite → hasta dónde queremos llegar
+#paso   → cuánto aumenta o disminuye
+
+#Ejercicio A
+for numA in range(3, 66, 1):
+    print(numA)
+else:
+    print("Se finalizo el conteo del ejercicio A")
+
+#Ejercicio B
+
+for numB in range(65, 2, -1):
+    print(numB)
+else:
+    print("Se finalizo el conteo del ejercicio B")
+
+#Ejercicio C
+
+for numC in range(12, 79, +3):
+    print(numC)
+else:
+    print("Se finalizo el conteo del ejercicio C")
+
+#Ejercicio D
+for numD in range(97, 1, -5):
+    print(numD)
+else:
+    print("Se finalizo el conteo del ejercicio D")
+
+#Ejercicio E
+for numE in range(3, 123, 6,):
+    print(numE)
+else:
+    print("Se finalizo el conteo del ejercicio E")
